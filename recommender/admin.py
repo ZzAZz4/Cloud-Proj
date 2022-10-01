f4edf69b25122b79bfbe966e7c0ce9ac07429c1d
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2022-09-30 21:09:28 -0500

    a

--- recommender/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from recommender.models import Movie, Similarity, OnlineLink
-
-admin.site.register(Movie)
-admin.site.register(Similarity)
-admin.site.register(OnlineLink)
